072a8ace93304dba4799831953d49c888ebc512c
shanacon <79785416+shanacon@users.noreply.github.com>
2021-03-05 12:59:21 +0800

    Update wake_process.c

--- code/wake_process.c
+++ code/wake_process.c
@@ -4,12 +4,6 @@
 #include<linux/wait.h>
 #include<linux/time.h>
 #include<linux/delay.h>
-extern bool flag1;
-extern bool flag2;
-extern bool flag3;
-extern wait_queue_head_t project2_queue_1;
-extern wait_queue_head_t project2_queue_2;
-extern wait_queue_head_t project2_queue_3;
 asmlinkage int clean_wait_queue(int x){
     if(x == 1){
     	flag1 = true;
