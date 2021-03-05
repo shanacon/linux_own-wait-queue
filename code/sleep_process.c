d01f0b5c3a0b42a6c84a3bdc17d3af9d54c83557
shanacon <79785416+shanacon@users.noreply.github.com>
2021-03-05 13:11:18 +0800

    Update sleep_process.c

--- code/sleep_process.c
+++ code/sleep_process.c
@@ -7,14 +7,16 @@
 DECLARE_WAIT_QUEUE_HEAD(project2_queue_1);
 DECLARE_WAIT_QUEUE_HEAD(project2_queue_2);
 DECLARE_WAIT_QUEUE_HEAD(project2_queue_3);
-bool flag = false;
+bool flag1 = false;
+bool flag2 = false;
+bool flag3 = false;;
 asmlinkage int enter_wait_queue(int x){
     if(x == 1)
-    	wait_event_interruptible(project2_queue_1,flag == true);
+    	wait_event_interruptible(project2_queue_1,flag1 == true);
     else if(x == 2)
-        wait_event_interruptible(project2_queue_2,flag == true);
+        wait_event_interruptible(project2_queue_2,flag2 == true);
     else if(x == 3)
-        wait_event_interruptible(project2_queue_3,flag == true);
+        wait_event_interruptible(project2_queue_3,flag3 == true);
     printk("test\n");
     return 1;
 }
